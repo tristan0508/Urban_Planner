5c5297d6f028d4bea0da0651e5179b391f1ae9fa
Tristan Wyatt <69435758+tristan0508@users.noreply.github.com>
2020-11-17 15:39:06 -0500

    phase 1 done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using Planner;
 
-namespace Planner
+namespace urban_planner
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Building FiveOneTwoEight = new Building("512 8th Avenue")
+        {
+            Stories = 100,
+            Width = 60.5,
+            Depth = 50.5
+        };
+        FiveOneTwoEight.HireDesigner("Design, LLC");
+        FiveOneTwoEight.Construct();
+        FiveOneTwoEight.Purchase("Bobby");
+        FiveOneTwoEight.DisplayBuildingInfo();
         }
     }
 }
